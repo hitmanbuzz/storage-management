818e59a8af00750666c61ae4850f059cf54ecb13
hitmanbuzz <hitmanbuzz@proton.me>
2026-06-15 13:35:50 +0530

    Refactor upload implementation
    Put all those upload implementation in upload.go file and make the upload route minimal

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"io"
 	"log/slog"
 	"net/http"
 	"storage-management/internal/storage"
@@ -32,95 +31,7 @@ func (r *route) Upload(ginCtx *gin.Context) {
 		return
 	}
 
-	upload := storage.NewUpload()
-
-	for {
-		part, err := mr.NextPart()
-		if err == io.EOF {
-			if upload.CurrFile != nil {
-				r.logger.Debug("eof reached", "add file", upload.CurrFile.Name)
-				upload.AddFile(upload.CurrFile)
-			}
-			break
-		}
-
-		if err != nil {
-			r.logger.Error("error", "reading part", err)
-			ginCtx.JSON(http.StatusInternalServerError, gin.H{"status": "multipart error"})
-			return
-		}
-
-		if upload.CurrFile != nil {
-			if upload.CurrFile.IsErr {
-				upload.AddFile(upload.CurrFile)
-				upload.CurrFile = nil
-			}
-		}
-
-		formName := part.FormName()
-
-		if formName == "" {
-			part.Close()
-			continue
-		}
-
-		switch formName {
-		case "user":
-			userByte, err := io.ReadAll(part)
-			part.Close()
-			if err != nil {
-				r.logger.Error("error", "failed to read username", err)
-				return
-			}
-
-			username := string(userByte)
-
-			if username == "" {
-				r.logger.Error("username is empty")
-				ginCtx.JSON(http.StatusBadRequest, gin.H{"status": "username is empty"})
-				return
-			}
-
-			if upload.User.Name == "" {
-				upload.SetUsername(username)
-			}
-
-			r.logger.Info("uploaded user", "name", username)
-		case "token":
-			// TODO: handle token
-		case "file":
-			fileName := part.FileName()
-			if fileName == "" {
-				part.Close()
-				continue
-			}
-
-			ext, _ := util.GetExtension(fileName)
-
-			if upload.CurrFile == nil {
-				upload.CurrFile = storage.NewFile(fileName, ext)
-				r.logger.Debug("update current file from nil", "file", fileName)
-			} else if upload.CurrFile.Name != fileName {
-				upload.AddFile(upload.CurrFile)
-				upload.CurrFile = storage.NewFile(fileName, ext)
-			}
-
-			writeSize, fullPath, err := storage.SaveFile(part, fileName)
-			part.Close()
-			if err != nil {
-				upload.CurrFile.IsErr = true
-				upload.CurrFile.Status = false
-				r.logger.Error("error", "upload file chunk", err)
-				ginCtx.JSON(http.StatusInternalServerError, gin.H{"status": "failed to upload file chunk"})
-				continue
-			}
-
-			upload.CurrFile.Size += writeSize
-			r.logger.Info("saved file chunk", "size", writeSize, "path", fullPath)
-		default:
-			part.Close()
-		}
-	}
-
-	ginCtx.JSON(http.StatusOK, upload)
+	upload := storage.NewHandleUpload(mr, r.logger)
+	upload.Do(ginCtx)
+	ginCtx.JSON(http.StatusOK, upload.GetUpload())
 }
--- internal/storage/upload.go
+++ internal/storage/upload.go
@@ -1,5 +1,16 @@
 package storage
 
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"mime/multipart"
+	"net/http"
+	"storage-management/internal/util"
+
+	"github.com/gin-gonic/gin"
+)
+
 type User struct {
 	Name  string `json:"username"`
 	Token string `json:"-"`
@@ -58,4 +69,148 @@ func (u *Upload) UpdateCurrFile(file *File) {
 		u.CurrFile = file
 		return
 	}
+}
+
+type HandleUpload struct {
+	reader *multipart.Reader
+	upload *Upload
+	logger *slog.Logger
+}
+
+func NewHandleUpload(mr *multipart.Reader, logger *slog.Logger) *HandleUpload {
+	return &HandleUpload{
+		reader: mr,
+		upload: NewUpload(),
+		logger: logger,
+	}
+}
+
+func (hu *HandleUpload) Do(ginCtx *gin.Context) {
+	for {
+		part, err := hu.reader.NextPart()
+		if err == io.EOF {
+			if hu.upload.CurrFile != nil {
+				hu.logger.Debug("eof reached", "add file", hu.upload.CurrFile.Name)
+				hu.upload.AddFile(hu.upload.CurrFile)
+			}
+			break
+		}
+
+		if err != nil {
+			hu.logger.Error("error", "reading part", err)
+			ginCtx.JSON(http.StatusInternalServerError, gin.H{"status": "multipart error"})
+		}
+
+		if hu.upload.CurrFile != nil {
+			if hu.upload.CurrFile.IsErr {
+				hu.upload.AddFile(hu.upload.CurrFile)
+				hu.upload.CurrFile = nil
+			}
+		}
+
+		form, err := hu.GetForm(part)
+		if err != nil {
+			ginCtx.JSON(http.StatusBadRequest, gin.H{"status": "form name is empty"})
+			part.Close()
+			continue
+		}
+
+		hu.HandleForm(ginCtx, form, part)
+	}
+}
+
+func (hu *HandleUpload) HandleForm(ginCtx *gin.Context, formName string, part *multipart.Part) {
+	switch formName {
+	case "user":
+		hu.HandleUser(ginCtx, part)
+	case "file":
+		hu.HandleFile(ginCtx, part)
+	}
+}
+
+func (hu *HandleUpload) HandleUser(ginCtx *gin.Context, part *multipart.Part) {
+	userName, err := hu.GetUser(part)
+	part.Close()
+	if err != nil {
+		hu.logger.Error("error", "failed to read username", err)
+		return
+	}
+
+	if len(userName) == 0 {
+		hu.logger.Error("username is empty")
+		ginCtx.JSON(http.StatusBadRequest, gin.H{"status": "username is empty"})
+		return
+	}
+
+	if len(hu.upload.User.Name) == 0 {
+		hu.upload.SetUsername(userName)
+	}
+
+	hu.logger.Info("uploaded user", "name", userName)
+}
+
+func (hu *HandleUpload) HandleFile(ginCtx *gin.Context, part *multipart.Part) {
+	fileName, err := hu.GetFile(part)
+	if err != nil {
+		hu.logger.Warn("empty filename")
+		part.Close()
+		return
+	}
+
+	ext, _ := util.GetExtension(fileName)
+	if hu.upload.CurrFile == nil {
+		hu.upload.CurrFile = NewFile(fileName, ext)
+	} else if hu.upload.CurrFile.Name != fileName {
+		hu.upload.AddFile(hu.upload.CurrFile)
+		hu.upload.CurrFile = NewFile(fileName, ext)
+	}
+
+	writeSize, fullPath, err := SaveFile(part, fileName)
+	part.Close()
+	if err != nil {
+		hu.upload.CurrFile.IsErr = true
+		hu.upload.CurrFile.Status = false
+		hu.logger.Error("error", "upload file chunk", err)
+		ginCtx.JSON(http.StatusInternalServerError, gin.H{"status": "failed to upload file chunk"})
+		return
+	}
+
+	hu.upload.CurrFile.Size += writeSize
+	hu.logger.Info("saved file chunk", "size", writeSize, "path", fullPath)
+}
+
+func (hu *HandleUpload) HandleAuth() {
+	// TODO: Still need to think how a simple auth need to be implemented
+}
+
+// --- Helper Methods ---
+
+func (hu *HandleUpload) GetForm(part *multipart.Part) (string, error) {
+	formName := part.FormName()
+	if len(formName) == 0 {
+		return "", fmt.Errorf("form name is empty")
+	}
+	return formName, nil
+}
+
+func (hu *HandleUpload) GetFile(part *multipart.Part) (string, error) {
+	fileName := part.FileName()
+	if len(fileName) == 0 {
+		return "", fmt.Errorf("file name is empty")
+	}
+	return fileName, nil
+}
+
+func (hu *HandleUpload) GetUser(part *multipart.Part) (string, error) {
+	userByte, err := io.ReadAll(part)
+	part.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return string(userByte), nil
+}
+
+func (hu *HandleUpload) GetUpload() *Upload {
+	return hu.upload
 }
